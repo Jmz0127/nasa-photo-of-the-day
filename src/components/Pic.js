682cda331480982a1836cfb9e68dfe8ca1bd6e77
Jmz0127 <jmezz1@icloud.com>
2021-11-04 16:56:11 -0400

    added styling to h1, h2, img and p

--- src/components/Pic.js
+++ src/components/Pic.js
@@ -1,16 +1,53 @@
 import React from 'react';
+import styled from 'styled-components';
 
 // THIS IS THE CHILD
 
+const StyledPic = styled.div`
+  width: 90.5%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding:5%;
+  background-color: lightskyblue;
+
+h1 {
+font-size: 35px;
+text-decoration: underline;
+font-family: Herculanum;
+color: purple;
+}
+
+
+h2 {
+font-size: 25px;
+font-family: fantasy;
+}
+
+
+p {
+font-size: 12px;
+padding-top: 10px;
+font-style: italic;
+font-family: monospace;
+  }
+
+
+img {
+    box-shadow: 12px 12px 2px 1px rgba(0, 0, 255, .2);
+}
+
+`
+
 const Pic = (props) => {
     return (     
-        <div>
+        <StyledPic>
             <h1>{props.data.title}</h1>
             <h2>{props.data.copyright}</h2>
             <h2>Date: {props.data.date}</h2>
             <img src={props.data.url} alt='space' />
             <p>{props.data.explanation}</p>
-        </div>
+        </StyledPic>
     )
 }
 
