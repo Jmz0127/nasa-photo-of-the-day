d0d5a591612a4fc38569c076f73cdb859861dfc2
Jmz0127 <jmezz1@icloud.com>
2021-11-03 20:04:06 -0400

    jon-mezzadri

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import React, { useState, useEffect } from "react"; //added usestate and useeffect hooks
 import "./App.css";
 import axios from 'axios'; //added axios since we need that to make a API request
+import { API_KEY } from './constants/indexconstants.js';
 
 
 export default function App() {
 
 const [space, setSpace] = useState([])
 useEffect(() => {
-  axios.get('https://api.nasa.gov/neo/rest/v1/feed?start_date=1987-07-01&end_date=1987-07-07&api_key=1nRJGHrgqQlJMuOlo2RTk8ZnMmFs1NkX35J5ZKhK') //used key
+  axios.get(`https://api.nasa.gov/neo/rest/v1/feed?start_date=1987-07-01&end_date=1987-07-07&api_key=${API_KEY}`) //used key
   .then(res => {
-    setSpace(res);
+    setSpace(res.data);
   })
   .catch(err => {
     console.error(err);
   })
 }, [])
 
-
-
+useEffect(() => {
+  axios.get(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`) //used key
+  .then(res => {
+    setSpace(res.data);
+  })
+  .catch(err => {
+    console.error(err);
+  })
+}, [])
 
 
 
