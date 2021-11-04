fd7bcc9f35ae8a3a1699e3def205af2ea7e0b0af
Jmz0127 <jmezz1@icloud.com>
2021-11-03 23:09:54 -0400

    finished full render

--- src/App.js
+++ src/App.js
@@ -1,41 +1,30 @@
 import React, { useState, useEffect } from "react"; //added usestate and useeffect hooks
 import "./App.css";
 import axios from 'axios'; //added axios since we need that to make a API request
-import { API_KEY, BEG_DATE, END_DATE } from './constants/indexconstants.js'; //added API info from constant folder
+import Pic from './components/Pic' //components need to be capitalized
 
 
 function App() {
 
-const [space, setSpace] = useState([])
+const [spaceData, setSpaceData] = useState([])
 useEffect(() => {
-  axios.get(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${BEG_DATE}&${END_DATE}=1987-07-07&api_key=${API_KEY}`) //used key
+  axios.get('https://api.nasa.gov/planetary/apod?api_key=1nRJGHrgqQlJMuOlo2RTk8ZnMmFs1NkX35J5ZKhK&date=2020-04-20') //used key
   .then(res => {
-    setSpace(res.data);
+    // console.log(res.data); 
+    setSpaceData(res.data);
+    
   })
   .catch(err => {
     console.error(err);
   })
 }, [])
 
-useEffect(() => {
-  axios.get(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`) //used key
-  .then(res => {
-    setSpace(res.data);
-  })
-  .catch(err => {
-    console.error(err);
-  })
-}, [])
-
-
+console.log(spaceData); //making sure state is set
 
 
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <Pic data = {spaceData}/>
     </div>
   );
 }
---
+++ src/components/Pic.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Pic = (props) => {
+    return (     
+        <div>
+            <h1>{props.data.title}</h1>
+            <h2>{props.data.copyright}</h2>
+            <h2>Date: {props.data.date}</h2>
+            <img src={props.data.url} alt='space' />
+            <p>{props.data.explanation}</p>
+        </div>
+    )
+}
+
+export default Pic;
